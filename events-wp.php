72cb6ab20153f6df73042baf99f782a2c4fcfaba
Austin Adams <austin@farmingtonwebdesign.net>
2015-06-25 17:09:46 -0700

    Create events-wp.php

---
+++ events-wp.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Events WP
+Plugin URI: https://github.com/JesusisFreedom/events-wp
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: Austin Adams
+Author URI: http://farmingtonwebdesign.net
+License: MIT
+*/
